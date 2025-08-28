9e137507245d1054d6205056d6d001bd51dfa0b7
HL-Neto <humbertolunaneto@gmail.com>
2025-08-27 21:00:44 -0300

    vers 0.2

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,3 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
+
 
 import './App.css'
 
@@ -5,8 +10,12 @@ function App() {
  
 
   return (
-    <>
-    </>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />      
+        <Route path="/home" element={<Home />} />  
+      </Routes>
+    </Router>
   )
 }
 
